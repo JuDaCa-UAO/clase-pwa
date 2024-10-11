429b025d2c372bee53ac6a7cc99847dabb8ca921
JuDaCa-UAO <juan_david.carvajal@uao.edu.co>
2024-10-11 18:30:34 -0500

    creación proyecto pwa

---
+++ src/componentes/Calculadora.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NumberInput from './NumberInput'
+
+const Calculadora = () => {
+  return (
+    <div>
+      Calculadora -pwa
+      <NumberInput />
+      <p></p>
+    </div>
+  )
+}
+
+export default Calculadora
